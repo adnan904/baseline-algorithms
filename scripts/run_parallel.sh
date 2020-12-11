1fe0f2fe5824f92940ce3ef5ca139b17d6203abe
Adnan Manzoor <adnan904@gmail.com>
2020-12-11 12:11:16 +0100

    Adding real-world traces files

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -2,9 +2,9 @@
 
 # (un)comment the Algorithm you want to use
 #algo='rs'
-#algo='lb'
-algo='sp'
+algo='lb'
+#algo='sp'
 
 # use GNU parallel to run multiple repetitions and scenarios in parallel
 # run from project root! (where Readme is)
-parallel --bar $algo ::: "--network" :::: scripts/network_files.txt ::: "--service_functions" :::: scripts/service_files.txt ::: "--config" :::: scripts/config_files.txt ::: "--iterations" ::: "1000" ::: "--seed" :::: scripts/30seeds.txt
+parallel --bar $algo ::: "--network" :::: scripts/network_files.txt ::: "--service_functions" :::: scripts/service_files.txt ::: "--config" :::: scripts/config_files.txt ::: "--iterations" ::: "200" ::: "--seed" :::: scripts/30seeds.txt
